5a1116bbaf5988ad591b1a798f23c9d387764a3a
ppscon <ppearson@me.com>
2023-09-25 13:24:00 +0100

    added artifact logic

--- generate_metadata_artifact.py
+++ generate_metadata_artifact.py
@@ -1,23 +1,62 @@
 import json
-import sys
+from datetime import datetime
 
-def generate_metadata_artifact(signature_file, timestamp_file, artifact_file):
+
+def generate_pretty_metadata_artifact(signature_file, timestamp_file, artifact_file):
+    # Read signature and timestamp
     with open(signature_file, 'r') as f:
         signature = f.read().strip()
 
     with open(timestamp_file, 'r') as f:
         timestamp = f.read().strip()
 
-    artifact = {
-        'signature': signature,
-        'timestamp': timestamp,
+    # Convert UNIX timestamp to human-readable date-time
+    human_readable_date = datetime.utcfromtimestamp(int(timestamp)).strftime('%Y-%m-%d %H:%M:%S UTC')
+
+    # Create metadata dictionary
+    metadata = {
+        "signature": signature,
+        "timestamp": timestamp,
+        "human_readable_date": human_readable_date
     }
 
-    with open(artifact_file, 'w') as f:
-        json.dump(artifact, f)
+    # Save as JSON
+    with open(artifact_file + '.json', 'w') as f:
+        json.dump(metadata, f, indent=4)
 
-if __name__ == "__main__":
-    signature_file = sys.argv[1]
-    timestamp_file = sys.argv[2]
-    artifact_file = sys.argv[3]
-    generate_metadata_artifact(signature_file, timestamp_file, artifact_file)
+    # Generate HTML
+    html_content = f"""
+    <html>
+    <head><title>Metadata Artifact</title></head>
+    <body>
+    <h1>Metadata Artifact</h1>
+    <table border="1">
+        <tr>
+            <th>Attribute</th>
+            <th>Value</th>
+        </tr>
+        <tr>
+            <td>Signature</td>
+            <td>{signature}</td>
+        </tr>
+        <tr>
+            <td>Timestamp</td>
+            <td>{timestamp} ({human_readable_date})</td>
+        </tr>
+    </table>
+    </body>
+    </html>
+    """
+
+    # Save as HTML
+    with open(artifact_file + '.html', 'w') as f:
+        f.write(html_content)
+
+
+# File paths
+signature_file = 'signature_file.txt'
+timestamp_file = 'timestamp_file.txt'
+artifact_file = 'metadata_artifact'
+
+# Generate the pretty metadata artifact
+generate_pretty_metadata_artifact(signature_file, timestamp_file, artifact_file)
