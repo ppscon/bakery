4eb7ebc6060662dcafb4c194faffd85eaaa2ab7f
ppscon <ppearson@me.com>
2023-10-30 17:42:17 +0000

    parse aqua-log

--- ci_scripts/assign_task.py
+++ ci_scripts/assign_task.py
@@ -2,7 +2,6 @@ import json
 import requests
 import os
 
-
 def read_aqua_scan_json(file_path):
     try:
         with open(file_path, 'r') as f:
@@ -15,14 +14,28 @@ def read_aqua_scan_json(file_path):
         print("Error decoding Aqua scan JSON file.")
         return None
 
-
 def send_teams_notification(assignee, aqua_data):
     webhook_url = os.environ.get("TEAMS_WEBHOOK_URL", "default_value_if_not_found")
 
     # Extract relevant details from Aqua scan data
-    vulnerabilities = aqua_data.get("vulnerabilities", [])
-    cve_ids = [v.get("name") for v in vulnerabilities]
-    severities = [v.get("severity") for v in vulnerabilities]
+    resources = aqua_data.get("resources", [])
+    all_vulnerabilities = []
+
+    for resource in resources:
+        vulnerabilities = resource.get("vulnerabilities", [])
+        for vuln in vulnerabilities:
+            cve_id = vuln.get("name")
+            severity = vuln.get("aqua_severity")
+            fix_version = vuln.get("fix_version")
+            nvd_url = vuln.get("nvd_url")
+            description = vuln.get("description")
+            all_vulnerabilities.append({
+                "CVE ID": cve_id,
+                "Severity": severity,
+                "Fix Version": fix_version,
+                "NVD URL": nvd_url,
+                "Description": description
+            })
 
     adaptive_card_content = {
         "type": "AdaptiveCard",
@@ -36,13 +49,27 @@ def send_teams_notification(assignee, aqua_data):
             },
             {
                 "type": "TextBlock",
-                "text": f"Found {len(cve_ids)} vulnerabilities. CVE IDs: {', '.join(cve_ids)}. Severities: {', '.join(severities)}"
+                "text": f"Found {len(all_vulnerabilities)} vulnerabilities."
             }
         ]
     }
 
+    for vuln in all_vulnerabilities:
+        adaptive_card_content["body"].append({
+            "type": "TextBlock",
+            "text": f"CVE ID: {vuln['CVE ID']}, Severity: {vuln['Severity']}, Fix: {vuln['Fix Version']}, [More info]({vuln['NVD URL']})"
+        })
+
     headers = {"Content-Type": "application/json"}
-    payload = json.dumps({"type": "message", "attachments": [{"contentType": "application/vnd.microsoft.card.adaptive", "content": adaptive_card_content}]})
+    payload = json.dumps({
+        "type": "message",
+        "attachments": [
+            {
+                "contentType": "application/vnd.microsoft.card.adaptive",
+                "content": adaptive_card_content
+            }
+        ]
+    })
 
     try:
         response = requests.post(webhook_url, headers=headers, data=payload)
@@ -51,7 +78,6 @@ def send_teams_notification(assignee, aqua_data):
     except requests.RequestException as e:
         print(f"Failed to send Teams notification: {e}")
         return None
-
 
 if __name__ == "__main__":
     assignee = 'Philip'
